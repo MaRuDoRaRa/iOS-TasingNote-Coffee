20c8f8dbff653446af1ab36d53e58022835bf95d
Maruyama Junichi <miraitechno@MiraitechnonoMacBook-Pro.local>
2017-09-13 23:43:13 +0900

    First Commit

---
+++ TastingNote-Coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TastingNote-Coffee
+//
+//  Created by Miraitechno Mac Book Pro on 2017/09/13.
+//  Copyright © 2017年 Junichi Maruyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
